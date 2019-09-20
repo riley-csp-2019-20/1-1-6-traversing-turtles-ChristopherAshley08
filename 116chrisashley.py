242018f2d29836bc032855bc51261f2515d32e7d
Chris Ashley <55500786+ChristopherAshley08@users.noreply.github.com>
2019-09-20 11:24:19 -0400

    Add files via upload

--- 116chrisashley.py
+++ 116chrisashley.py
@@ -6,18 +6,23 @@ import turtle as trtl
 my_turtles = []
 
 # use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold", "red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
+  new_colors=turtle_colors.pop()
+  t.fillcolor(new_colors)
+  t.pencolor(new_colors)
   my_turtles.append(t)
 #  
 startx = 0
 starty = 0
-
+direction=90
 #
 for t in my_turtles:
+    t.setheading(direction)
+    direction=t.heading() + 30
     t.penup()
     t.goto(startx, starty)
     t.pendown()
@@ -25,10 +30,8 @@ for t in my_turtles:
     t.forward(50)
     startx = t.xcor()  
     starty = t.ycor() 
-    new_colors=turtle_colors.pop()
-    t.fillcolor(new_colors)
-    t.pencolor(new_colors)
-    t.color(new_colors)
+    
+
 
 wn = trtl.Screen()
 wn.mainloop()
